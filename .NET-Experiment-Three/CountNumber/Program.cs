d51b96797cd8ce6e27183ab4a6b6034991d32b9a
Jason Yoo <1154083659@qq.com>
2021-04-15 22:15:14 +0800

    add file

---
+++ .NET-Experiment-Three/CountNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CountNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,Cnt=0;
+            
+            Console.WriteLine("请输入一个数：");
+            x=int.Parse(Console.ReadLine());
+            for (; x != 0; x >>= 1)
+            {
+                if ((x & 1)==1)
+                {
+                    Cnt++;
+                }
+            }
+            Console.WriteLine("{0}", Cnt);
+        }
+    }
+}
